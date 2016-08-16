539eadff0297e5a483c3665009e5fac51e46cd05
WISS1_0103 <WISS1_0103@192.168.1.14>
2016-08-16 18:48:01 +0900

    20c引数の変更

--- src/co/wiss1/control/W0020Control.java
+++ src/co/wiss1/control/W0020Control.java
@@ -22,7 +22,7 @@ public class W0020Control extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	public void doPost(HttpServletRequest request, HttpServletResponse response, String categoryid)
 		throws IOException, ServletException {
 		System.out.println("doPost start");
 
